f0aa9e1e1e6971c7443ec3f719b09795e7fb172f
Henrik Schuller <hgs716@gmail.com>
2020-04-20 19:27:44 +0200

    Add files via upload

--- edit-ekstra.js
+++ edit-ekstra.js
@@ -5,13 +5,10 @@ $('a[data-toggle="pill"]').on("shown.bs.tab", function (e) {
   var target = $(e.target).attr("href");
   scrollTo(0, document.body.scrollHeight);
 });
-/*$("#mainview").click(function () {
+$("#mainview").click(function () {
   scrollTo(0, 0);
 });
-*/
-$("#blocker").click(function () {
-  scrollTo(0, 0);
-});
+
 $(window).scroll(function () {
   if ($(window).scrollTop() > 20) {
     if (!dark) {
